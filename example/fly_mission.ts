b4956ac2faa5232eb0e2ab9f1b982066e9e4bf74
Andrea Pomente <pomons90@gmail.com>
2020-01-14 10:57:44 +0100

    add fly mission example + new docs  + autors in examples

--- example/fly_mission.ts
+++ example/fly_mission.ts
@@ -1,4 +1,17 @@
-import { Mavsdk, System, Telemetry, MissionItem, Mission, Action } from "../lib/addon";
+/**
+ * @file fly_mission.ts
+ *
+ * @brief Demonstrates how to Add & Fly Waypoint missions using the MAVJS.
+ * The example is summarised below:
+ * 1. Adds mission items.
+ * 2. Starts mission from first mission item.
+ * 3. Illustrates Pause/Resume mission item.
+ * 4. Exits after the mission is accomplished.
+ *
+ * @author Andrea Pomente <andrea.pomente@nodriver.ai>,
+ * @date 2020-
+ * */
+import { Mavsdk, MissionItem, Mission, Action } from "../lib/addon";
 
 function usage(bin_name: string): void
 {
@@ -41,9 +54,6 @@ let main = async () => {
 
     let mavsdk = new Mavsdk();
 
-    let discovered_system: boolean = false;
-
-
     let connection_result: Mavsdk.ConnectionResult;
     if (argc == 1) {
         const connection_url: string = process.argv[2];
@@ -77,6 +87,9 @@ let main = async () => {
         process.exit();
     })
 
+    // We don't need to specifiy the UUID if it's only one system anyway.
+    // If there were multiple, we could specify it with:
+    // mavsdk.system(uuid: number);
     let system = mavsdk.system();
     
     let telemetry = system.telemetry();
@@ -155,21 +168,14 @@ let main = async () => {
         MissionItem.CameraAction.STOP_PHOTO_INTERVAL));
 
     console.log("Uploading mission...");
-    /*{
-        let prom = function() {
-            return new Promise((resolve, reject) => {
-                mission.upload_mission_async(mission_items, (result) => {
-                    if (result != Mission.Result.SUCCESS) {
-                        console.log(`Mission upload failed ${result}, exiting.`)
-                        process.exit();
-                    }
-                    resolve()
-                })
-            });
+    {
+        let result = await mission.upload_mission_async(mission_items)
+        
+        if (result != Mission.Result.SUCCESS) {
+            console.log(`Mission upload failed ${result}, exiting.`)
+            process.exit();
         }
-
-        await prom();
-    }*/
+    }   
     console.log("Mission uploaded.");
 
     // Arm vehicle
@@ -181,6 +187,89 @@ let main = async () => {
         process.exit();
     }
 
+    let want_to_pause: boolean = false;
+    // Before starting the mission, we want to be sure to subscribe to the mission progress.
+    mission.subscribe_progress((progress) => {
+        console.log(`Mission status update: ${progress.current} / ${progress.total}`);
+
+        if (progress.current >= 2) {
+            // We can only set a flag here. If we do more request inside the callback,
+            // we risk blocking the system.
+            want_to_pause = true;
+        }
+    });
+
+    {
+        console.log("Starting mission.");
+        let result = await mission.start_mission_async();
+
+        if (result != Mission.Result.SUCCESS) {
+            console.log(`Mission start failed: ${result}`);
+            process.exit();
+        } else {
+            console.log("Started mission.");
+        }
+    }
+
+    while (!want_to_pause) {
+        await sleep(1000);
+    }
+
+    {
+        console.log("Pausing mission...");
+        let result = await mission.pause_mission_async();
+
+        if (result != Mission.Result.SUCCESS) {
+            console.log(`Failed to pause mission: ${result}`);
+            process.exit();
+        } else {
+            console.log("Mission paused.");
+        }
+    }
+
+    // Pause for 5 seconds.
+    await sleep(5000);
+
+    // Then continue.
+    {
+        console.log("Resuming mission...");
+        let result = await mission.start_mission_async();
+
+        if (result != Mission.Result.SUCCESS) {
+            console.log(`Failed to resume mission: ${result}`);
+            process.exit();
+        } else {
+            console.log("Resumed mission.");
+        }
+    }
+
+    while (!mission.mission_finished()) {
+        await sleep(1000);
+    }
+
+    {
+        // We are done, and can do RTL to go home.
+        console.log("Commanding RTL...");
+        let result = action.return_to_launch();
+
+        if (result != Action.Result.SUCCESS) {
+            console.log(`Failed to command RTL: ${result}`);
+            process.exit();
+        } else {
+            console.log("Commanded RTL.");
+        }
+    }
+
+    // We need to wait a bit, otherwise the armed state might not be correct yet.
+    await sleep(2000);
+
+    while (telemetry.armed()) {
+        // Wait until we're done.
+        await sleep(1000);
+    }
+
+
+    console.log("Disarmed, exiting.");
    
     process.exit();
 }
